b15e86a1a1db266f38242d96c7afdf561463e169
PaulFL808 <paulferrada578@gmail.com>
2026-09-07 11:16:56 -0300

    Corregir identificador de la app para Firebase

--- profesor/app/build.gradle.kts
+++ profesor/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.asistencia.profesor"
+        applicationId = "android_65.fc"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
